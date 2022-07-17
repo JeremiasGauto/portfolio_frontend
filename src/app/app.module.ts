ed546eb011c0aae93e9d5d319303b41809c49769
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2022-07-17 18:51:38 -0300

    acercaDe agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    IntroComponent
+    IntroComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
