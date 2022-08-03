6f42181c9ef80ba3c72e728275a46c77d603b945
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2022-08-02 21:06:11 -0300

    Se agregan las nuevas experiencias a la tabla frontend

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Experiencia } from 'src/app/model/experiencia';
 import { ServExperienciaService } from 'src/app/service/serv-experiencia.service';
 import { TokenService } from 'src/app/service/token.service';
@@ -11,7 +12,7 @@ import { TokenService } from 'src/app/service/token.service';
 export class ExperienciaComponent implements OnInit {
   exper: Experiencia[] = [];
 
-  constructor(private servExperiencia: ServExperienciaService, private tokenService: TokenService) { }
+  constructor(private servExperiencia: ServExperienciaService, private tokenService: TokenService, router: Router) { }
   isLogged = false;
   
   ngOnInit(): void {
