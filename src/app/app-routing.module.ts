45a65b5344b0ddb7344111b859a1b59c04b0e5df
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2022-08-03 12:06:32 -0300

    editar educacion agregado, funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/estudios/neweducacion.component';
+import { EditeducacionComponent } from './components/estudios/editeducacion.component';
 
 
 const routes: Routes = [
   {path:'', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'nuevaexp', component: NewExperienciaComponent },
-  {path:'editexp/:id',component: EditExperienciaComponent},
+  { path: 'editexp/:id', component: EditExperienciaComponent },
+  { path: 'nuevaedu', component: NeweducacionComponent },
+  {path: 'editedu/:id', component: EditeducacionComponent}
   
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { interceptorProvider, InterceptorService } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/estudios/neweducacion.component';
+import { EditeducacionComponent } from './components/estudios/editeducacion.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +40,9 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     HomeComponent,
     LoginComponent,
     NewExperienciaComponent,
-    EditExperienciaComponent
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent
   ],
   imports: [
     BrowserModule,
